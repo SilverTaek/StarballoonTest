c59f1db4b1f5f936cf6df4c49557a01a11a14926
rudus1012 <rudus1012@naver.com>
2020-12-03 18:57:19 +0900

    Final

--- main.js
+++ main.js
@@ -9,13 +9,20 @@ function readURL(input) {
       $('.file-upload-image').attr('src', e.target.result);
       $('.file-upload-content').show();
 
-      $('.image-title').html(input.files[0].name);
+
+      // $('.image-title').html(input.files[0].name);
     };
 
     reader.readAsDataURL(input.files[0]);
+    $('.container').show();
     init().then(() => {
       predict();
-    })
+      $('.container').hide();
+      $('.image-title-wrap').show();
+
+      $('.main').show();
+    });
+
 
   } else {
     removeUpload();
@@ -23,9 +30,12 @@ function readURL(input) {
 }
 
 function removeUpload() {
-  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-  $('.file-upload-content').hide();
-  $('.image-upload-wrap').show();
+  // $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+  // $('.image-title-wrap').hide();
+  // $('.file-upload-content').hide();
+  // $('.image-upload-wrap').show();
+  // $('.main').hide();
+  location.reload();
 }
 $('.image-upload-wrap').bind('dragover', function () {
   $('.image-upload-wrap').addClass('image-dropping');
@@ -33,3 +43,19 @@ $('.image-upload-wrap').bind('dragover', function () {
 $('.image-upload-wrap').bind('dragleave', function () {
   $('.image-upload-wrap').removeClass('image-dropping');
 });
+
+function gender() {
+  if (gender1.style.display == 'block' && boy.style.display == 'none') {
+    gender2.style.display = 'block'
+    gender1.style.display = "none"
+    girl.style.display = "none"
+    boy.style.display = 'block'
+    const URL = "";
+  } else if (gender1.style.display == 'none' && boy.style.display == 'block') {
+    gender2.style.display = 'none'
+    gender1.style.display = 'block'
+    girl.style.display = "block"
+    boy.style.display = 'none'
+  }
+
+}
